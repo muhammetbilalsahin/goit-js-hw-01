2c5f9f87e990f0a104dca784cf3179059dcdc5e2
sahinbilisim <shnblsm@gmail.com>
2025-02-15 23:16:16 +0300

    task-3 fixed

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@ function getElementWidth(content, padding, border) {
   const contentValue = parseFloat(content);
   const paddingValue = parseFloat(padding);
   const borderValue = parseFloat(border);
-  const totalWidth = contentValue + paddingValue + borderValue;
-  return `${totalWidth}`;
+  const totalWidth = (paddingValue + borderValue) * 2 + contentValue;
+  return totalWidth;
 }
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
